8ff12357395d3280c0833973bc1c9508a8a48110
rick <ricardolopezbonaguro@gmail.com>
2022-04-02 17:01:07 -0300

    trying to implement horizontal cards

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,67 +1,14 @@
 import React from 'react';
 import "./styles/Home.css";
-import Logo from "./logo.png";
+import { NavBar} from "./NavBar";
 
 
 export function Home() {
-    const logoWidthHeight = "50";
-    const lineHeight = {
-        lineHeight: "50px",
-        paddingLeft: "10px",
-        fontSize: "35px",
-        fontFamily: "work sans",
-        color: "black",
-        fontWeight: "bold",
-    };
 
-
-
-    const NavBar = (props) => {
-        return (
-            <div className="navbar navbar-expand-md navbar-light bg-warning">
-                <a href="/" className="navbar-brand" >
-                    <span><img src={Logo} alt="logo" width={logoWidthHeight} height={logoWidthHeight} className="d-inline-block align-top"/></span>
-                    <span style={lineHeight}>Fixit</span>
-                </a>
-
-                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#main-nav" aria-controls="main-nav" aria-expanded="false" aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon"></span>
-
-                </button>
-
-                <div className="collapse navbar-collapse justify-content-end align-end" id="main-nav">
-                    <ul className="navbar-nav">
-                        <li className="nav-item">
-                            <a className="nav-link" href="/">Notifications</a>
-                        </li>
-
-
-                        <li className="nav-item dropdown">
-                            <a className="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button"
-                               data-bs-toggle="dropdown" aria-expanded="false">
-                                Hello, {props.firstName}
-                            </a>
-                            <ul className="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-                                <li><a className="dropdown-item" href="#">Hired Services</a></li>
-                                <li><a className="dropdown-item" href="#">My Profile</a></li>
-                                <li><a className="dropdown-item" href="#">Settings</a></li>
-                            </ul>
-                        </li>
-
-
-
-                        <li className="nav-item ms-2 d-md-inline">
-                            <a className="btn btn-secondary" href="/">Log out</a>
-                        </li>
-                    </ul>
-
-                </div>
-
-
-
-            </div>
-        )
+    const imgSrc = (__filename) => {
+        return `${process.env.PUBLIC_URL}/assets/images/dog.jpg`;
     }
+
 
     const TopSearchBanner = () => {
         return(
@@ -96,24 +43,43 @@ export function Home() {
         );
     }
 
+    const JobResult = () => {
+        return(
+            <div className="row my-5 align-items-center justify-content-center">
+                <div className="col-8 col-lg-4 col-xl-3">
+                    <div className="card mb-3">
+                        <div className="row g-0">
+                            <div className="col-md-4">
+                                <img src={`${process.env.PUBLIC_URL}/assets/images/dog.jpg`} className="img-fluid rounded-start" alt="dog"/>
+                            </div>
+
+                            <div className="col-md-8">
+                                <div className="card-body">
+                                    <h5 className="card-title">Card title</h5>
+                                    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                    <a href="#" className="btn btn-primary">Go somewhere</a>
+                                </div>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+        );
+
+
+    }
+
+
     return (
         <div>
 
             <NavBar firstName="Luis" />
             <TopSearchBanner />
+            <JobResult />
 
-            <div className="row my-5 align-items-center justify-content-center">
-                <div className="col-8 col-lg-4 col-xl-3">
-                    <div className="card">
-                        <div className="card-body">
-                            <h5 className="card-title">Luis</h5>
-                            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                            <a href="#" className="btn btn-primary">Go somewhere</a>
-                        </div>
-                    </div>
-                </div>
 
-            </div>
+
 
 
 
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,54 @@
+import Logo from "./logo.png";
+import React from "react";
+
+export const NavBar = (props) => {
+    const logoWidthHeight = "50";
+    const lineHeight = {
+        lineHeight: "50px",
+        paddingLeft: "10px",
+        fontSize: "35px",
+        fontFamily: "work sans",
+        color: "black",
+        fontWeight: "bold",
+    };
+
+    return (
+        <div className="navbar navbar-expand-md navbar-light bg-warning">
+            <a href="/" className="navbar-brand" >
+                <span><img src={Logo} alt="logo" width={logoWidthHeight} height={logoWidthHeight} className="d-inline-block align-top"/></span>
+                <span style={lineHeight}>Fixit</span>
+            </a>
+
+            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#main-nav" aria-controls="main-nav" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
+
+            </button>
+
+            <div className="collapse navbar-collapse justify-content-end align-end" id="main-nav">
+                <ul className="navbar-nav">
+                    <li className="nav-item">
+                        <a className="nav-link" href="/">Notifications</a>
+                    </li>
+
+
+                    <li className="nav-item dropdown">
+                        <a className="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button"
+                           data-bs-toggle="dropdown" aria-expanded="false">
+                            Hello, {props.firstName}
+                        </a>
+                        <ul className="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                            <li><a className="dropdown-item" href="#">Hired Services</a></li>
+                            <li><a className="dropdown-item" href="#">My Profile</a></li>
+                            <li><a className="dropdown-item" href="#">Settings</a></li>
+                        </ul>
+                    </li>
+
+                    <li className="nav-item ms-2 d-md-inline">
+                        <a className="btn btn-secondary" href="/">Log out</a>
+                    </li>
+                </ul>
+            </div>
+
+        </div>
+    )
+}
